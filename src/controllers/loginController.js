1f70b693888cb482f02ed47e08f1014891be4461
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-15 10:09:06 -0500

    refactor: now the controller renders the home page view without needing to reload the page

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,11 @@
 import { hashPassword } from "../utils/commons.js";
+import homeView from "../views/homeSection.js";
+import { homeController } from "./homeController.js";
 const userNameSaved = "Melissa";
 const userPasswordSaved = "7db5026dd502a59031b7a57d1f9956b03d11b35d9f90d1af825fa8da9f491960";
 
-export async function loginController(loginForm) {
+export async function loginController(loginForm, appContainer, renderApp) {
+
     const errorBadged = document.getElementById("error-message");
     const successBadged = document.getElementById("success-message");
 
@@ -11,19 +14,38 @@ export async function loginController(loginForm) {
 
     let currentBadged;
 
-    if (inputUsername !== userNameSaved || inputPassword !== userPasswordSaved) {
+    if (
+        inputUsername !== userNameSaved ||
+        inputPassword !== userPasswordSaved
+    ) {
+
         errorBadged.classList.remove("hidden");
         currentBadged = errorBadged;
+
         console.error("Usuario o contraseña no coinciden");
+
     } else {
+
         successBadged.classList.remove("hidden");
         currentBadged = successBadged;
-        console.log("Redirigiendo...");
-        localStorage.setItem("auth", "true")
-    };
 
-    setTimeout(()=> {
+        localStorage.setItem("auth", "true");
+
+        setTimeout(() => {
+            renderApp();
+        }, 2000);
+    }
+
+    setTimeout(() => {
         currentBadged.classList.add("hidden");
         loginForm.reset();
     }, 2000);
-};
+}
+
+
+export function logoutController(renderApp) {
+
+    localStorage.removeItem("auth");
+
+    renderApp();
+}
