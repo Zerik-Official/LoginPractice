f16c657cf1f2b9633cd4fce9938ce6c60fb777a1
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-15 09:57:12 -0500

    refactor: encapsulating the rendering of views into a single function

--- src/main.js
+++ src/main.js
@@ -2,25 +2,40 @@ import './style.css'
 import loginView from './views/loginSection.js'
 import { loginController } from './controllers/loginController.js';
 import homeView from './views/homeSection.js';
+import { homeController } from './controllers/homeController.js';
 
 const appContainer = document.getElementById("app");
 
-let userLogin = localStorage.getItem("auth");
+function renderApp() {
 
+    const userLogin = localStorage.getItem("auth");
 
-document.addEventListener("DOMContentLoaded", () => {
     if (!userLogin) {
-        appContainer.innerHTML = loginView(appContainer)
+
+        appContainer.innerHTML = loginView();
+
         const formLogin = document.getElementById("form-login");
 
         formLogin.addEventListener("submit", (event) => {
             event.preventDefault();
-            loginController(formLogin);
 
-        })
+            loginController(
+                formLogin,
+                appContainer,
+                renderApp
+            );
+        });
+
     } else {
 
-        appContainer.innerHTML = homeView(appContainer)
+        appContainer.innerHTML = homeView();
 
+        homeController(
+            appContainer,
+            userLogin,
+            renderApp
+        );
     }
-});
+}
+
+document.addEventListener("DOMContentLoaded", renderApp);
