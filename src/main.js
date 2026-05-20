9fad9b216129fedce7b3b101042400f98cf59068
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-20 07:16:07 -0500

    refactor: integrate router into main entry point

--- src/main.js
+++ src/main.js
@@ -3,39 +3,35 @@ import loginView from './views/loginSection.js'
 import { loginController } from './controllers/loginController.js';
 import homeView from './views/homeSection.js';
 import { homeController } from './controllers/homeController.js';
+import Router from './router/index.js';
 
-const appContainer = document.getElementById("app");
-
-function renderApp() {
-
-    const userLogin = localStorage.getItem("auth");
-
-    if (!userLogin) {
-
-        appContainer.innerHTML = loginView();
-
-        const formLogin = document.getElementById("form-login");
-
-        formLogin.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            loginController(
-                formLogin,
-                appContainer,
-                renderApp
-            );
-        });
-
-    } else {
-
-        appContainer.innerHTML = homeView();
-
-        homeController(
-            appContainer,
-            userLogin,
-            renderApp
-        );
+const router = Router({
+  "/login": {
+    action({ navigate, app }) {
+      app.innerHTML = loginView();
+      const formLogin = document.getElementById("form-login");
+      formLogin.addEventListener("submit", (event) => {
+        event.preventDefault();
+        loginController(formLogin, navigate);
+      });
     }
-}
+  },
+  "/productos": {
+    protected: true,
+    action({ navigate, app }) {
+      app.innerHTML = homeView();
+      homeController(navigate);
+    }
+  },
+  "/": {
+    action({ navigate }) {
+      if (localStorage.getItem("auth")) {
+        navigate("/productos");
+      } else {
+        navigate("/login");
+      }
+    }
+  }
+});
 
-document.addEventListener("DOMContentLoaded", renderApp);
+document.addEventListener("DOMContentLoaded", () => router.handleRoute());
