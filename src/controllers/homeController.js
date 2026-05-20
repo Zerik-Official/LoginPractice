d21f3f1812de83c42fcf79e078076e0106fc7aa9
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-20 07:16:15 -0500

    refactor: home controller uses navigate for logout

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,21 +1,12 @@
 import { logoutController } from "./loginController.js";
-import loginView from "../views/loginSection.js";
 import { productController } from "./productController.js";
 
-export function homeController(appContainer, userLogin, renderApp) {
+export function homeController(navigate) {
     const closeSession = document.getElementById("logout-btn");
-    const productContainer = document.getElementById("products-container");
 
-    closeSession.addEventListener("click", ()=> {
-        const result = logoutController(renderApp);
-
-        console.log("resultado de intento de cierre de sesión:", result)
-
-        if (result) {
-            appContainer.innerHTML = loginView(appContainer)
-        }
-    })
+    closeSession.addEventListener("click", () => {
+        logoutController(navigate);
+    });
 
     productController();
-
 }
