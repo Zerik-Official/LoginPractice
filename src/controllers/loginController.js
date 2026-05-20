622cd66b2533d02f27bfaa3ebffd78a7a264b9bb
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-20 07:16:10 -0500

    refactor: login controller uses navigate instead of renderApp

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,51 +1,33 @@
 import { hashPassword } from "../utils/commons.js";
-import homeView from "../views/homeSection.js";
-import { homeController } from "./homeController.js";
+
 const userNameSaved = "Melissa";
 const userPasswordSaved = "7db5026dd502a59031b7a57d1f9956b03d11b35d9f90d1af825fa8da9f491960";
 
-export async function loginController(loginForm, appContainer, renderApp) {
-
+export async function loginController(loginForm, navigate) {
     const errorBadged = document.getElementById("error-message");
     const successBadged = document.getElementById("success-message");
-
     const inputUsername = loginForm[0].value;
     const inputPassword = await hashPassword(String(loginForm[1].value));
-
     let currentBadged;
 
-    if (
-        inputUsername !== userNameSaved ||
-        inputPassword !== userPasswordSaved
-    ) {
-
+    if (inputUsername !== userNameSaved || inputPassword !== userPasswordSaved) {
         errorBadged.classList.remove("hidden");
         currentBadged = errorBadged;
-
         console.error("Usuario o contraseña no coinciden");
-
     } else {
-
         successBadged.classList.remove("hidden");
         currentBadged = successBadged;
-
         localStorage.setItem("auth", "true");
-
-        setTimeout(() => {
-            renderApp();
-        }, 2000);
+        setTimeout(() => { navigate("/productos"); }, 2000);
     }
 
     setTimeout(() => {
-        currentBadged.classList.add("hidden");
+        currentBadged?.classList.add("hidden");
         loginForm.reset();
     }, 2000);
 }
 
-
-export function logoutController(renderApp) {
-
+export function logoutController(navigate) {
     localStorage.removeItem("auth");
-
-    renderApp();
+    navigate("/login");
 }
